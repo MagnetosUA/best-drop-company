f666530305c79bcdc8c1ce5cf38db928eb14ff23
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-08-04 16:33:05 +0300

    Create Product and Ord(for order) entities

---
+++ src/Droparea/DropBundle/Entity/Ord.php
@@ -0,0 +1,572 @@
+<?php
+
+namespace Droparea\DropBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ord
+ *
+ * @ORM\Table(name="ord")
+ * @ORM\Entity(repositoryClass="Droparea\DropBundle\Repository\OrdRepository")
+ */
+class Ord
+{
+    const NEW_OREDER = 'Новый';
+    const IN_PROCESSING = 'В обработке';
+    const NERPOKE = 'Недозвон';
+    const CONFIRMED = 'Подтвержденный';
+    const REJECTED = 'Отклоненный';
+    const SHIPPED = 'Отправленный';
+    const NON_PURCHASE = 'Невыкуп';
+    const RANSOM = 'Выкуп';
+    const NOT_SENT_FOR_PROCESSING = 'Не отправлен в обработку';
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="orderNumber", type="integer", unique=true)
+     */
+    private $orderNumber;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="changed", type="datetime")
+     */
+    private $changed;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="product", type="string", length=255)
+     */
+    private $product;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone", type="string", length=255)
+     */
+    private $phone;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="status", type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ыsaleAmount", type="string", length=255)
+     */
+    private $ыsaleAmount;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="purchaseAmount", type="string", length=255)
+     */
+    private $purchaseAmount;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="�income", type="string", length=255)
+     */
+    private $�income;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="treatmentType", type="string", length=255)
+     */
+    private $treatmentType;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="settlementType", type="string", length=255)
+     */
+    private $settlementType;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="clientPhone", type="string", length=255)
+     */
+    private $clientPhone;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="clientName", type="string", length=255)
+     */
+    private $clientName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="�waybill", type="string", length=255)
+     */
+    private $�waybill;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="deliveryAddress", type="string", length=1000)
+     */
+    private $deliveryAddress;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="comment", type="string", length=1500)
+     */
+    private $comment;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="source", type="string", length=255)
+     */
+    private $source;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set orderNumber
+     *
+     * @param integer $orderNumber
+     *
+     * @return Ord
+     */
+    public function setOrderNumber($orderNumber)
+    {
+        $this->orderNumber = $orderNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get orderNumber
+     *
+     * @return int
+     */
+    public function getOrderNumber()
+    {
+        return $this->orderNumber;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return Ord
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set changed
+     *
+     * @param \DateTime $changed
+     *
+     * @return Ord
+     */
+    public function setChanged($changed)
+    {
+        $this->changed = $changed;
+
+        return $this;
+    }
+
+    /**
+     * Get changed
+     *
+     * @return \DateTime
+     */
+    public function getChanged()
+    {
+        return $this->changed;
+    }
+
+    /**
+     * Set product
+     *
+     * @param string $product
+     *
+     * @return Ord
+     */
+    public function setProduct($product)
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    /**
+     * Get product
+     *
+     * @return string
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param string $phone
+     *
+     * @return Ord
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     *
+     * @return Ord
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set ыsaleAmount
+     *
+     * @param string $ыsaleAmount
+     *
+     * @return Ord
+     */
+    public function setыsaleAmount($ыsaleAmount)
+    {
+        $this->ыsaleAmount = $ыsaleAmount;
+
+        return $this;
+    }
+
+    /**
+     * Get ыsaleAmount
+     *
+     * @return string
+     */
+    public function getыsaleAmount()
+    {
+        return $this->ыsaleAmount;
+    }
+
+    /**
+     * Set purchaseAmount
+     *
+     * @param string $purchaseAmount
+     *
+     * @return Ord
+     */
+    public function setPurchaseAmount($purchaseAmount)
+    {
+        $this->purchaseAmount = $purchaseAmount;
+
+        return $this;
+    }
+
+    /**
+     * Get purchaseAmount
+     *
+     * @return string
+     */
+    public function getPurchaseAmount()
+    {
+        return $this->purchaseAmount;
+    }
+
+    /**
+     * Set �income
+     *
+     * @param string $�income
+     *
+     * @return Ord
+     */
+    public function set�income($�income)
+    {
+        $this->�income = $�income;
+
+        return $this;
+    }
+
+    /**
+     * Get �income
+     *
+     * @return string
+     */
+    public function get�income()
+    {
+        return $this->�income;
+    }
+
+    /**
+     * Set treatmentType
+     *
+     * @param string $treatmentType
+     *
+     * @return Ord
+     */
+    public function setTreatmentType($treatmentType)
+    {
+        $this->treatmentType = $treatmentType;
+
+        return $this;
+    }
+
+    /**
+     * Get treatmentType
+     *
+     * @return string
+     */
+    public function getTreatmentType()
+    {
+        return $this->treatmentType;
+    }
+
+    /**
+     * Set settlementType
+     *
+     * @param string $settlementType
+     *
+     * @return Ord
+     */
+    public function setSettlementType($settlementType)
+    {
+        $this->settlementType = $settlementType;
+
+        return $this;
+    }
+
+    /**
+     * Get settlementType
+     *
+     * @return string
+     */
+    public function getSettlementType()
+    {
+        return $this->settlementType;
+    }
+
+    /**
+     * Set clientPhone
+     *
+     * @param string $clientPhone
+     *
+     * @return Ord
+     */
+    public function setClientPhone($clientPhone)
+    {
+        $this->clientPhone = $clientPhone;
+
+        return $this;
+    }
+
+    /**
+     * Get clientPhone
+     *
+     * @return string
+     */
+    public function getClientPhone()
+    {
+        return $this->clientPhone;
+    }
+
+    /**
+     * Set clientName
+     *
+     * @param string $clientName
+     *
+     * @return Ord
+     */
+    public function setClientName($clientName)
+    {
+        $this->clientName = $clientName;
+
+        return $this;
+    }
+
+    /**
+     * Get clientName
+     *
+     * @return string
+     */
+    public function getClientName()
+    {
+        return $this->clientName;
+    }
+
+    /**
+     * Set �waybill
+     *
+     * @param string $�waybill
+     *
+     * @return Ord
+     */
+    public function set�waybill($�waybill)
+    {
+        $this->�waybill = $�waybill;
+
+        return $this;
+    }
+
+    /**
+     * Get �waybill
+     *
+     * @return string
+     */
+    public function get�waybill()
+    {
+        return $this->�waybill;
+    }
+
+    /**
+     * Set deliveryAddress
+     *
+     * @param string $deliveryAddress
+     *
+     * @return Ord
+     */
+    public function setDeliveryAddress($deliveryAddress)
+    {
+        $this->deliveryAddress = $deliveryAddress;
+
+        return $this;
+    }
+
+    /**
+     * Get deliveryAddress
+     *
+     * @return string
+     */
+    public function getDeliveryAddress()
+    {
+        return $this->deliveryAddress;
+    }
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     *
+     * @return Ord
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * Set source
+     *
+     * @param string $source
+     *
+     * @return Ord
+     */
+    public function setSource($source)
+    {
+        $this->source = $source;
+
+        return $this;
+    }
+
+    /**
+     * Get source
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return $this->source;
+    }
+}
+
---
+++ src/Droparea/DropBundle/Entity/Product.php
@@ -0,0 +1,345 @@
+<?php
+
+namespace Droparea\DropBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="product")
+ * @ORM\Entity(repositoryClass="Droparea\DropBundle\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="productCode", type="string", length=255, unique=true)
+     */
+    private $productCode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="category", type="string", length=255)
+     */
+    private $category;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="weight", type="string", length=255)
+     */
+    private $weight;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cost", type="string", length=255)
+     */
+    private $cost;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="recomendedCost", type="string", length=255)
+     */
+    private $recomendedCost;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="returnCost", type="string", length=255)
+     */
+    private $returnCost;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="valuta", type="string", length=255)
+     */
+    private $valuta;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set productCode
+     *
+     * @param string $productCode
+     *
+     * @return Product
+     */
+    public function setProductCode($productCode)
+    {
+        $this->productCode = $productCode;
+
+        return $this;
+    }
+
+    /**
+     * Get productCode
+     *
+     * @return string
+     */
+    public function getProductCode()
+    {
+        return $this->productCode;
+    }
+
+    /**
+     * Set category
+     *
+     * @param string $category
+     *
+     * @return Product
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return string
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     *
+     * @return Product
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return Product
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set weight
+     *
+     * @param string $weight
+     *
+     * @return Product
+     */
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    /**
+     * Get weight
+     *
+     * @return string
+     */
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
+    /**
+     * Set cost
+     *
+     * @param string $cost
+     *
+     * @return Product
+     */
+    public function setCost($cost)
+    {
+        $this->cost = $cost;
+
+        return $this;
+    }
+
+    /**
+     * Get cost
+     *
+     * @return string
+     */
+    public function getCost()
+    {
+        return $this->cost;
+    }
+
+    /**
+     * Set recomendedCost
+     *
+     * @param string $recomendedCost
+     *
+     * @return Product
+     */
+    public function setRecomendedCost($recomendedCost)
+    {
+        $this->recomendedCost = $recomendedCost;
+
+        return $this;
+    }
+
+    /**
+     * Get recomendedCost
+     *
+     * @return string
+     */
+    public function getRecomendedCost()
+    {
+        return $this->recomendedCost;
+    }
+
+    /**
+     * Set returnCost
+     *
+     * @param string $returnCost
+     *
+     * @return Product
+     */
+    public function setReturnCost($returnCost)
+    {
+        $this->returnCost = $returnCost;
+
+        return $this;
+    }
+
+    /**
+     * Get returnCost
+     *
+     * @return string
+     */
+    public function getReturnCost()
+    {
+        return $this->returnCost;
+    }
+
+    /**
+     * Set valuta
+     *
+     * @param string $valuta
+     *
+     * @return Product
+     */
+    public function setValuta($valuta)
+    {
+        $this->valuta = $valuta;
+
+        return $this;
+    }
+
+    /**
+     * Get valuta
+     *
+     * @return string
+     */
+    public function getValuta()
+    {
+        return $this->valuta;
+    }
+}
+
