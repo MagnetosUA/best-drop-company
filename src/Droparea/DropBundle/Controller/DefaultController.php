0528b92e087570c218fa6e0849c58eb90dfc1478
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-08-17 21:43:14 +0300

    Delete unusable code from DefaultController

--- src/Droparea/DropBundle/Controller/DefaultController.php
+++ src/Droparea/DropBundle/Controller/DefaultController.php
@@ -70,13 +70,9 @@ class DefaultController extends Controller
     public function ajaxNewPostAction(Request $request)
     {
         if ($city = $request->request->get('destination')) {
-//            $this->get('session')->set('cityDescription', $city);
-
             $addressDb = $this->get('get.new.post.address.from.db');
-//            $address = $this->get('FetchNewPostAddress');
             $warehouses = $addressDb->getWarehouses($city);
             $area = $addressDb->getArea($city);
-//            $responseData = [$city];
             $ar = [$city, $area, $warehouses];
             $ar = json_encode($ar);
             return new Response($ar);
@@ -86,21 +82,8 @@ class DefaultController extends Controller
     public function ordersAction(Request $request)
     {
         $addressDb = $this->get('get.new.post.address.from.db');
-//        $address = $this->get('FetchNewPostAddress');
-        //$address->getWarehousesOnline();die;
-//        $war = $addressDb->getWarehouses("Черкассы");
-//        print_r($war);die;
-
         $form = $this->createForm(OrderClientType::class);
-
-//        $address = $this->get('GetNewPostAddressFromDB');
-//        $addressDb->getCities();
-
         $staticSitiesFull = $addressDb->getCities();
-//        echo "<pre>";
-//        print_r($staticSitiesFull);
-//        echo "</pre>";die;
-
         return $this->render('@Drop/Pages/orders.html.twig', [
             'cities' => $staticSitiesFull,
             'form' => $form->createView(),
