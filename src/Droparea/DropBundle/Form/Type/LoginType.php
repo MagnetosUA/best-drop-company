a5f4cfefd56ef55eb0591b26251be038285ebf23
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-08-22 16:00:01 +0300

    Fixed some code

--- src/Droparea/DropBundle/Form/Type/LoginType.php
+++ src/Droparea/DropBundle/Form/Type/LoginType.php
@@ -5,10 +5,6 @@ namespace Droparea\DropBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class LoginType extends AbstractType
