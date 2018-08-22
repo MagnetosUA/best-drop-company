0173ad1faf0bdee6c1f621516c8285db93481293
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-08-22 11:37:48 +0300

    Create SequrityController and login route

---
+++ src/Droparea/DropBundle/Controller/SecurityController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Droparea\DropBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    public function loginAction()
+    {
+
+    }
+}
