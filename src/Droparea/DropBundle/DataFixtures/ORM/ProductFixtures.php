2829d4e4b7e75451ac518e7931c1128f3c4395c1
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-12-05 22:45:31 +0200

    Change images folder in config

--- src/Droparea/DropBundle/DataFixtures/ORM/ProductFixtures.php
+++ src/Droparea/DropBundle/DataFixtures/ORM/ProductFixtures.php
@@ -8,18 +8,17 @@ use Droparea\DropBundle\Entity\Product;
 
 class ProductFixtures extends Fixture
 {
-    public $images = ['ola.jpeg', 'gps.jpeg', 'husq.jpeg'];//, 'tv.jpeg', 'iphone.jpeg'];
+    public $images = ['iphone.jpeg', 'gps.jpeg', 'tv.jpeg', 'a8e9f822bc3fbcf45f0956e67f69c742.jpeg'];//, 'tv.jpeg', 'iphone.jpeg'];
 
     public $description = 'Особенности Соединение с телефоном по Bluetooth Встроенная память 8 ГБ в комплекте Детектирование камер и радаров полиции по GPS-базе с регулярным автоматическим обновлением Голосовое сопровождение На русском языке Поворотный кронштейн Батарея регистратора 30 минут Bluetooth Bluetooth Smart';
 
     public function load(ObjectManager $manager)
     {
-
-        // create 20 products! Bam!
+        // create 10 products! Bam!
         for ($i = 0; $i < 10; $i++) {
             $product = new Product();
             $product->setName('Товар '.$i);
-            $product->setCost(mt_rand(10, 100));
+            $product->setCost(mt_rand(300, 1500));
             $product->setImages($this->images);
             $product->setDescription($this->description);
             $category = $this->getReference('category'.mt_rand(0, 3));
@@ -29,7 +28,7 @@ class ProductFixtures extends Fixture
             $product->setReturnCost(50);
             $product->setValuta('UAH');
             $product->setWeight('1 kg');
-            $product->setPurveyor('Юлька');
+            $product->setPurveyor('Julia');
 
             $manager->persist($product);
         }
